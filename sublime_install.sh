f8a388b09ae3eed0a479f4bb2c0fe531f350d4a1
Sam Dye <sam.re.dye@gmail.com>
2014-06-16 08:04:12 -0400

    Use /tmp

--- sublime_install.sh
+++ sublime_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download some fonts - Fira, Inconsolata, Source Code Pro
-mkdir ~/fonts && cd ~/fonts
+mkdir /tmp/installscript && cd /tmp/installscript
 wget http://www.carrois.com/wordpress/downloads/fira_3_1/FiraFonts3108.zip
 wget http://www.levien.com/type/myfonts/Inconsolata.otf
 wget http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip
@@ -19,11 +19,9 @@ if [[ "$(uname)" == "Linux" ]] ; then
   fc-cache -fv
 
   # Download Sublime
-  cd ~
   wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.tar.bz2
   tar -vxjf "Sublime Text 2.0.2.tar.bz2"
   mv "Sublime Text 2" /opt/
-  rm "Sublime Text 2.0.2.tar.bz2"
 
   # Sublime preferences
   ln -nsf sublime/Preferences.sublime-settings "~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings"
@@ -39,12 +37,10 @@ else if [[ "$(uname)" == "Darwin" ]] ; then
   cp SourceCodePro_FontsOnly-1.017/OTF/* ~/Library/Fonts
 
   # Download Sublime
-  cd ~
   wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
   hdiutil mount "Sublime Text 2.0.2.dmg"
   cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" /Applications/
   hdiutil unmount "/Volumes/Sublime Text 2/"
-  rm -f "Sublime Text 2.0.0.dmg"
 
   # Sublime preferences
   ln -nsf sublime/Preferences.sublime-settings "~/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings"
@@ -53,5 +49,5 @@ else if [[ "$(uname)" == "Darwin" ]] ; then
 
 fi
 
-cd ~ && rm -rf ~/fonts
+cd ~ && rm -rf /tmp/installscript
 
