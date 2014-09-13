deca294daaa204ee6b145c46279bf42d36bea1ca
Sam Dye <sam.re.dye@gmail.com>
2014-09-13 18:09:22 -0400

    Fix font_install script and no longer install Fira Mono

--- font_install.sh
+++ font_install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-mkdir /tmp/installscript && cd /tmp/installscript
+mkdir -p /tmp/installscript && cd /tmp/installscript
 
-wget http://www.carrois.com/wordpress/downloads/fira_3_1/FiraFonts3108.zip
 wget http://www.levien.com/type/myfonts/Inconsolata.otf
 wget http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip
 wget https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf
 
-unzip FiraFonts3108.zip
 unzip SourceCodePro_FontsOnly-1.017.zip
 
 if [[ "$(uname)" == "Linux" ]] ; then
@@ -15,7 +13,7 @@ if [[ "$(uname)" == "Linux" ]] ; then
   wget http://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
   wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 
-  mkdir -p /usr/share/fonts/opentype/FiraMono /usr/share/fonts/opentype/Inconsolata /usr/share/fonts/opentype/source-code-pro /usr/share/fonts/opentype/inconsolata-for-powerline /usr/share/fonts/opentype/powerline-symbols
+  mkdir -p /usr/share/fonts/opentype/Inconsolata /usr/share/fonts/opentype/source-code-pro /usr/share/fonts/opentype/inconsolata-for-powerline /usr/share/fonts/opentype/powerline-symbols
   mv FiraFonts3108/FiraMono3108/OTF/ /usr/share/fonts/opentype/FiraMono/
   mv Inconsolata.otf /usr/share/fonts/opentype/Inconsolata/
   mv SourceCodePro_FontsOnly-1.017/OTF/ /usr/share/fonts/opentype/source-code-pro/
@@ -32,10 +30,9 @@ if [[ "$(uname)" == "Linux" ]] ; then
     mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
   fi
 
-else if [[ "$(uname)" == "Darwin" ]] ; then
+elif [[ "$(uname)" == "Darwin" ]] ; then
 
   mkdir -p ~/Library/Fonts
-  mv FiraFonts3108/FiraMono*/OTF/* ~/Library/Fonts
   mv Inconsolata.otf ~/Library/Fonts
   mv SourceCodePro_FontsOnly-1.017/OTF/* ~/Library/Fonts
   mv "Inconsolata for Powerline.otf" ~/Library/Fonts
@@ -43,3 +40,4 @@ else if [[ "$(uname)" == "Darwin" ]] ; then
 fi
 
 cd ~ && rm -rf /tmp/installscript
+
