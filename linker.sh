e9f990083d2783dd44c6513eaac3d8efcdb8aa5c
Sam Dye <sam.re.dye@gmail.com>
2014-11-14 22:21:19 -0500

    Make the linker script work recursively

--- linker.sh
+++ linker.sh
@@ -2,28 +2,48 @@
 # Links everything in home/ to ~/, does sanity checks.
 # By Simon Eskildsen (github.com/Sirupsen)
 
+home_dir="home"
+home_dir_len=${#home_dir}
+
+if [[ "$(uname)" == "Linux" ]]; then
+  md5="md5sum"
+else
+  md5="md5"
+fi
+
 function symlink {
   ln -nsf $1 $2
 }
 
-for file in home/.[^.]*; do
-  path="$(pwd)/$file"
-  base=$(basename $file)
-  target="$HOME/$(basename $file)"
+function recursive_symlink {
+  for file in $1/*; do
+    path="$(pwd)/$file"
+    base=$(basename $file)
+    target="$HOME/$(awk -v path=$file -v len=$home_dir_len 'BEGIN{ print substr(path, len+2); }')"
 
-  if [[ -h $target && ($(readlink $target) == $path)]]; then
-    echo -e "\x1B[90m~/$base is symlinked to your dotfiles.\x1B[39m"
-  elif [[ -f $target && $(md5 $path) == $(md5 $target) ]]; then
-    echo -e "\x1B[32m~/$base exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
-    symlink $path $target
-  elif [[ -a $target ]]; then
-    read -p "\x1B[33m~/$base exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
+    if [[ -d $path ]]; then
+      mkdir -p $target
+      recursive_symlink $file
+    else
+      if [[ -h $target && ($(readlink $target) == $path)]]; then
+        echo -e "$target is symlinked to your dotfiles."
+      elif [[ -f $target && $($md5 $path) == $($md5 $target) ]]; then
+        echo -e "$target exists and was identical to your dotfile.  Overriding with symlink."
+        #symlink $path $target
+      elif [[ -a $target ]]; then
+        read -p "$target exists and differs from your dotfile. Override?  [yn]" -n 1
 
-    if [[ $REPLY =~ [yY]* ]]; then
-      symlink $path $target
+        if [[ $REPLY =~ [yY]* ]]; then
+          symlink $path $target
+        fi
+      else
+        echo -e "$target does not exist. Symlinking to dotfile."
+        symlink $path $target
+      fi
     fi
-  else
-    echo -e "\x1B[32m~/$base does not exist. Symlinking to dotfile.\x1B[39m"
-    symlink $path $target
-  fi
-done
+  done
+}
+
+shopt -s dotglob
+recursive_symlink $home_dir
+shopt -u dotglob
